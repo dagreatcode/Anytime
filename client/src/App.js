4db7a8d43513507f4a65a5c526c2aad74d9a14e2
dagreatcode <dagreatcode@gmail.com>
2023-09-17 16:25:54 -0400

    added react

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          "ANYTIME" will be up and running momentarily.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/dagreatcode/Anytime"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
