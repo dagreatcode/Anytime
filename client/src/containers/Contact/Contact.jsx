1152a9a000892c2c2e9a089171ed245aacd7f493
dagreatcode <dagreatcode@gmail.com>
2023-09-19 22:41:46 -0400

    updated the contact page

--- client/src/containers/Contact/Contact.jsx
+++ client/src/containers/Contact/Contact.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
 // import PropTypes from 'prop-types'
 
 // props
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <div className="container">
+      <div className="row" style={{textAlign: "center"}}>
+      <div className="">Contact</div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+ 
+      </div>  
+      <Button className="container" style={{border: "15px solid",width: "auto", display: "flex", justifyContent: "center"}}>Send</Button>
+    </div>
+    </>
   )
 }
 
