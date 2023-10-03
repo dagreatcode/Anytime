2a2817dca9adfdc0f22374f7122effc18f7354cd
dagreatcode <dagreatcode@gmail.com>
2023-10-03 15:24:40 -0400

    updating the banner

--- client/src/containers/Home/Banner/Banner.jsx
+++ client/src/containers/Home/Banner/Banner.jsx
@@ -1,20 +1,46 @@
-import Img from "./new.png"
-import './Banner.css'
+import { Link } from "react-router-dom";
+import Img from "./new.png";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-   <>
-    <div>
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      }}>
-      <img className="hero" src={Img} alt="anytime" style={{ width: "100%"}}/>
+    <>
+      <div class="card text-center">
+        <div class="card-header">50% Discount Off Limited Time</div>
+        <div class="card-body">
+          <img
+            className="hero"
+            src={Img}
+            alt="anytime"
+            style={{ width: "100%" }}
+          />
+          <a
+            href="tel:(1)(7702120143)"
+            className="btn btn-dark btn-sm"
+            tabindex="-1"
+            role="button"
+            aria-disabled="true"
+          >
+            Call
+          </a>
+          <Link
+            to="/Contact"
+            className="btn btn-dark btn-sm"
+            tabindex="-1"
+            role="button"
+            aria-disabled="true"
+          >
+            Appointment
+          </Link>
+          <h5 class="card-title">Contact Us Anytime & We'll Be There In No Time.</h5>
+          <p class="card-text">
+            Here is why you should choose us...
+          </p>
+        </div>
+        <div class="card-footer text-muted">We Are Here When You Need Us 24/7.</div>
       </div>
-    </div>
-   </>
-  )
-}
+    </>
+  );
+};
 
-export default Banner
+export default Banner;
