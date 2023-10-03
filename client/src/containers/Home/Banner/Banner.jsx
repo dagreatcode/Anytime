ff19362dfcad4ace720d71c82aeb38954655f81e
dagreatcode <dagreatcode@gmail.com>
2023-10-03 15:35:02 -0400

    updating Banner

--- client/src/containers/Home/Banner/Banner.jsx
+++ client/src/containers/Home/Banner/Banner.jsx
@@ -4,42 +4,46 @@ import "./Banner.css";
 
 const Banner = () => {
   return (
-    <>
-      <div class="card text-center">
-        <div class="card-header">50% Off Discount Limited Time Offer</div>
-        <div class="card-body">
-          <img
-            className="hero"
-            src={Img}
-            alt="anytime"
-            style={{ width: "100%" }}
-          />
-          <a
-            href="tel:(1)(7702120143)"
-            className="btn btn-dark btn-sm"
-            tabindex="-1"
-            role="button"
-            aria-disabled="true"
-          >
-            Call
-          </a>
-          <Link
-            to="/Contact"
-            className="btn btn-dark btn-sm"
-            tabindex="-1"
-            role="button"
-            aria-disabled="true"
-          >
-            Appointment
-          </Link>
-          <h5 class="card-title">Contact Us Anytime & We'll Be There In No Time.</h5>
-          <p class="card-text">
-            Here is why you should choose us...
-          </p>
+    <div className="container">
+      <div className="row">
+        <div className="card text-center">
+          <div className="card-header">50% Off Discount Limited Time Offer</div>
+          <div className="card-body">
+            <img
+              classNameName="hero"
+              src={Img}
+              alt="anytime"
+              style={{ width: "100%" }}
+            />
+            <a
+              href="tel:(1)(7702120143)"
+              classNameName="btn btn-dark btn-sm"
+              tabindex="-1"
+              role="button"
+              aria-disabled="true"
+            >
+              Call
+            </a>
+            <Link
+              to="/Contact"
+              classNameName="btn btn-dark btn-sm"
+              tabindex="-1"
+              role="button"
+              aria-disabled="true"
+            >
+              Appointment
+            </Link>
+            <h5 className="card-title">
+              Contact Us Anytime & We'll Be There In No Time.
+            </h5>
+            <p className="card-text">Here is why you should choose us...</p>
+          </div>
+          <div className="card-footer text-muted">
+            We Are Here When You Need Us 24/7.
+          </div>
         </div>
-        <div class="card-footer text-muted">We Are Here When You Need Us 24/7.</div>
       </div>
-    </>
+    </div>
   );
 };
 
--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Banner from './Banner/Banner'
-import './Home.css'
+import React from "react";
+import Banner from "./Banner/Banner";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
       <Banner />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
